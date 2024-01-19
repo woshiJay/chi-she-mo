9d17020548acb1fa4b17260bb8c006bcb9790565
Sattish Shewkani <spshewkani@gmail.com>
2024-01-19 18:09:33 +0800

    added authentication route - backend

--- server.js
+++ server.js
@@ -7,6 +7,109 @@ const port = 5501;
 app.use(express.json());
 app.use(cors());
 
+// ----------------------------------------------------------------------
+// Initializing of Firebase Admin SDK
+// ----------------------------------------------------------------------
+const admin = require('firebase-admin')
+const { getAuth } = require("firebase-admin/auth");
+const serviceAccount = require("./serviceAccount.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://chi-se-mo-default-rtdb.asia-southeast1.firebasedatabase.app/"
+});
+const db = admin.database();
+
+// ----------------------------------------------------------------------
+// Initializing of Firebase SDK
+// ----------------------------------------------------------------------
+const firebase = require("firebase/app");
+const firebaseAuth = require("firebase/auth");
+const firebaseConfig = {
+  apiKey: "AIzaSyBTybXLML4LVemnnav7vAvvlwbd9XE0WQc",
+  authDomain: "chi-se-mo.firebaseapp.com",
+  databaseURL: "https://chi-se-mo-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "chi-se-mo",
+  storageBucket: "chi-se-mo.appspot.com",
+  messagingSenderId: "460919636016",
+  appId: "1:460919636016:web:07051c3573000ca1b12df5"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// ----------------------------------------------------------------------
+// Authentication
+// ----------------------------------------------------------------------
+
+// Sign up route
+app.post('/signup', async (req, res) => {
+
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+      res.status(400).json({ alert: 'Please ensure that all fields are filled.' });
+      return;
+  }
+  const auth = getAuth();
+
+  const userRecord = await auth.createUser({
+      email: email,
+      password: password
+  })
+      .catch((error) => {
+          if (error.code === 'auth/email-already-exists') {
+              res.status(400).json({ alert: 'Email already exists! Please proceed to Login.' });
+          } else {
+              res.status(400).json({ alert: error.code })
+          }
+      })
+
+  const userId = userRecord.uid;
+  const userRef = db.ref(`users/${userId}`);
+  await userRef.set({ username: username })
+  res.status(200).json({ redirect: '/src/login.html' });
+});
+
+// Sign in route
+app.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+  const firebaseAuthentication = firebaseAuth.getAuth();
+
+  firebaseAuth.signInWithEmailAndPassword(firebaseAuthentication, email, password)
+      .then((userCredential) => {
+          // Signed in
+          res.status(200).json({ message: "User signed in successfully!", uid: userCredential.user.uid })
+      })
+      .catch((error) => {
+          res.status(401).json({ alert: 'Invalid email or password! Please try again.' });
+      });
+});
+
+// Sign out route
+app.get('/signout', async (req, res) => {
+  firebaseAuth.getAuth().signOut()
+      .then(() => {
+          res.status(200).json({ message: "User signed out successfully!" })
+      })
+      .catch((error) => {
+      });
+});
+
+// ----------------------------------------------------------------------
+// Database Routes
+// ----------------------------------------------------------------------
+
+
+
+// YEE TAO
+
+
+
+
+
+// ----------------------------------------------------------------------
+// Places API
+// ----------------------------------------------------------------------
+
 app.post("/getRestaurants", async (req, res) => {
   const { lat, lon } = req.body;
 
