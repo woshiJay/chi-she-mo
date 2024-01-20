5fef7016bd40e40c9f322b69dc1ef29acec87b84
Sattish Shewkani <spshewkani@gmail.com>
2024-01-20 17:14:02 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-window.onload = () => {
-    const userId = sessionStorage.getItem('userId');
-    if (!userId) {
-        window.location.href = '/src/login.html';
-    } else if (userId) {
-        fetch(`http://localhost:5501/get-username?uid=${userId}`)
-        .then(resp => resp.json())
-        .then(data => {
-            // gets username and updates HTML to display username
-        })
-        .catch(error => {
-            console.error("Error:", error);
-        })
-    }
-}
+// window.onload = () => {
+//     const userId = sessionStorage.getItem('userId');
+//     if (!userId) {
+//         window.location.href = '/src/login.html';
+//     } else if (userId) {
+//         fetch(`http://localhost:5501/get-username?uid=${userId}`)
+//         .then(resp => resp.json())
+//         .then(data => {
+//             // gets username and updates HTML to display username
+//         })
+//         .catch(error => {
+//             console.error("Error:", error);
+//         })
+//     }
+// }
