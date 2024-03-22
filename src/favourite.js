428ac717f19aba2a65bcf93564e58338af9ed035
Jay <sengkit100@gmail.com>
2024-03-22 10:49:07 +0800

    file update

--- src/favourite.js
+++ src/favourite.js
@@ -1,9 +1,19 @@
 window.onload = () => {
     const userId = sessionStorage.getItem('userId');
     if (!userId) {
-      window.location.href = '/src/login.html';
+      window.location.href = '/src/pages/login.html';
     } else {
-      fetch(`http://localhost:5501/api/user_restaurants?userID=${userId}`)
+        fetch(`http://localhost:5501/get-username?uid=${userId}`)
+        .then(resp => resp.json())
+        .then(data => {
+            // Assuming 'data' has a 'username' property with the user's name
+            const username = data.username;
+            const usernameElement = document.querySelector('.user-disp');
+            // Replace the placeholder text with the actual username
+            usernameElement.textContent = `Welcome Back, ${username}.`;
+            
+        return fetch(`http://localhost:5501/api/user_restaurants?userID=${userId}`)
+    })
       .then(resp => resp.json())
       .then(restaurants => {
         const container = document.querySelector('.row.justify-content-center'); // The container where the rows will be appended
