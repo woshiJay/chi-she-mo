90b488c475d1d874ad559caa14c52d2c41c6b70d
Jay <sengkit100@gmail.com>
2024-10-15 14:58:50 +0800

    onload bug fixes

--- src/favourite.js
+++ src/favourite.js
@@ -1,39 +1,38 @@
-const baseURL = window.location.origin;
-
+// const baseURL = window.location.origin;
 window.onload = async () => {
   const userId = sessionStorage.getItem('userId');
   if (!userId) {
     window.location.href = '/src/pages/login.html';
   } else {
     try {
-      const resp = await fetch(`${baseURL}/get-username?uid=${userId}`);
-      const data = await resp.json();
-      const username = data.username;
-      const usernameElement = document.querySelector('.user-disp');
-      usernameElement.textContent = `Welcome Back, ${username}.`;
-
-      const resp2 = await fetch(`${baseURL}/api/user_restaurants?userID=${userId}`);      
-      const restaurants = await resp2.json();
+      const resp = await fetch(`${baseURL}/api/user_restaurants?userID=${userId}`);
+      const restaurants = await resp.json();
       const container = document.querySelector('.row.justify-content-center');
       container.innerHTML = '';
-      restaurants.forEach(restaurant => {
-        const cardHtml = `
-          <div class="container">
-            <div class="col">
-              <div class="card my-2 p-2">
-                <div class="card-body d-flex justify-content-between align-items-center">
-                  <a href="https://www.google.com/maps/place/?q=place_id:${restaurant.placeID}" class="resLink text-decoration-none text-dark" target=_blank>
-                    <span class="resName fw-medium">${restaurant.resName}</span>
-                  </a>
+
+      if (restaurants.length === 0) {
+        container.innerHTML = '<p>No favourite restaurants found.</p>';
+      } else {
+        restaurants.forEach((restaurant) => {
+          const cardHtml = `
+            <div class="container">
+              <div class="col">
+                <div class="card my-2 p-2">
+                  <div class="card-body d-flex justify-content-between align-items-center">
+                    <a href="https://www.google.com/maps/place/?q=place_id:${restaurant.placeID}" 
+                       class="resLink text-decoration-none text-dark" target=_blank>
+                      <span class="resName fw-medium">${restaurant.resName}</span>
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        `;
-        container.insertAdjacentHTML('beforeend', cardHtml);
-      });
+          `;
+          container.insertAdjacentHTML('beforeend', cardHtml);
+        });
+      }
     } catch (error) {
-      console.error("Error:", error);
+      console.error("Error fetching restaurants:", error);
     }
   }
 };
